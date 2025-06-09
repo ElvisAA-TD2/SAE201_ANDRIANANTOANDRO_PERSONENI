2bc677f80ea85412a0c2544b262641685141d555
ElvisAA-TD2 <Elvis-Aaron.Andrianantoandro@etu.univ-smb.fr>
2025-06-09 18:43:26 +0200

    Filtre du Uc MesCommandes

--- SAE201_ANDRIANANTOANDRO_PERSONENI/UserControls/MesCommandes.xaml.cs
+++ SAE201_ANDRIANANTOANDRO_PERSONENI/UserControls/MesCommandes.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,11 +26,56 @@ namespace SAE201_ANDRIANANTOANDRO_PERSONENI.UserControls
         public MesCommandes()
         {
             InitializeComponent();
+            dg_mesCommandes.Items.Filter = RechercheCommandes;
         }
+
+        private bool RechercheCommandes(object obj)
+        {
+            if (obj is not Commande uneCommande)
+                return false;
+
+            string filtreRaisonSociale = tb_raisonSociale_revendeur.Text.Trim();
+            string filtreDate = tb_dateCreation_commande.Text.Trim();
+
+            if (String.IsNullOrWhiteSpace(filtreRaisonSociale) && String.IsNullOrWhiteSpace(filtreDate))
+                return true;
+
+            bool raisonSocialeOk = true;
+            if (!string.IsNullOrWhiteSpace(filtreRaisonSociale))
+                raisonSocialeOk = uneCommande.UnRevendeur.RaisonSociale.Contains(filtreRaisonSociale, StringComparison.OrdinalIgnoreCase);
+
+            bool dateCommandeOk = true;
+            if (!string.IsNullOrWhiteSpace(filtreDate))
+            {
+                if (Regex.IsMatch(filtreDate, "^[0-9]{4}$"))
+                {
+                    dateCommandeOk = uneCommande.DateCommande.Year.ToString() == filtreDate;
+                }
+                else if (Regex.IsMatch(filtreDate, "^[0-9]{2}/[0-9]{4}$"))
+                {
+                    string mois = uneCommande.DateCommande.Month.ToString("D2");
+                    string annee = uneCommande.DateCommande.Year.ToString();
+                    dateCommandeOk = $"{mois}/{annee}" == filtreDate;
+                }
+                else
+                {
+                    dateCommandeOk = uneCommande.DateCommande.ToShortDateString() == filtreDate;
+                }
+            }
+
+            return raisonSocialeOk && dateCommandeOk;
+        }
+
 
         private void VoirRecapitulatif_Click(object sender, RoutedEventArgs e)
         {
             VoirDetailsCommandes?.Invoke(this, (Commande)((Button)sender).Tag);
+        }
+
+
+        private void TextBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(dg_mesCommandes.ItemsSource).Refresh();
         }
     }
 }
